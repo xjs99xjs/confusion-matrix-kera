9cc7ec65ed005a0dde19d286ff49c9d3c6f9e5e8
xjs99xjs <53007256+xjs99xjs@users.noreply.github.com>
2019-08-09 22:14:23 +0800

    Add files via upload

---
+++ matrix result.R
@@ -0,0 +1,27 @@
+> confusionMatrix(Confumatrix, positive = "1") 
+Confusion Matrix and Statistics
+
+labels
+pre    0    1
+0 1316  726
+1  793 1456
+
+Accuracy : 0.646           
+95% CI : (0.6315, 0.6603)
+No Information Rate : 0.5085          
+P-Value [Acc > NIR] : < 2e-16         
+
+Kappa : 0.2914          
+
+Mcnemar's Test P-Value : 0.09038         
+                                          
+            Sensitivity : 0.6673          
+            Specificity : 0.6240          
+         Pos Pred Value : 0.6474          
+         Neg Pred Value : 0.6445          
+             Prevalence : 0.5085          
+         Detection Rate : 0.3393          
+   Detection Prevalence : 0.5241          
+      Balanced Accuracy : 0.6456          
+                                          
+       'Positive' Class : 1   
